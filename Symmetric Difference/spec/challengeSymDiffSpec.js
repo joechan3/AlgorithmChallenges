d2e506ed1d9d2b64328281e2daaef6fdb4fb68ab
Joseph Chan <joechan3@gmail.com>
2016-06-02 23:36:33 -0600

    Solve but not yet refactor

--- Symmetric Difference/spec/challengeSymDiffSpec.js
+++ Symmetric Difference/spec/challengeSymDiffSpec.js
@@ -8,21 +8,63 @@ describe('Symmetric Difference algorithm tests', function () {
     });
   
     it('sym([1, 2, 3], [5, 2, 1, 4]) should return [3, 4, 5].', function () {
-        //foo = sym([1, 2, 3], [5, 2, 1, 4]);
-        foo = [3, 4, 5];
+        foo = sym([1, 2, 3], [5, 2, 1, 4]);
+        //foo = [3, 4, 5];
         expect(foo).toEqual([3, 4, 5]);
     });
     
     it('sym([1, 2, 3], [5, 2, 1, 4]) should contain only three elements.', function () {
-        //foo = sym([1, 2, 3], [5, 2, 1, 4]);
-        foo = [3, 4, 5];
+        foo = sym([1, 2, 3], [5, 2, 1, 4]);
+        //foo = [3, 4, 5];
         expect(foo.length).toBe(3);
     });
     
     it('sym([1, 2, 5], [2, 3, 5], [3, 4, 5]) should return [1, 4, 5].', function () {
-        //foo = sym([1, 2, 5], [2, 3, 5], [3, 4, 5]);
-        foo = [1, 4, 5];
+        foo = sym([1, 2, 5], [2, 3, 5], [3, 4, 5]);
+        //foo = [1, 4, 5];
         expect(foo).toEqual([1, 4, 5]);
+    });
+    
+    it('sym([1, 2, 5], [2, 3, 5], [3, 4, 5]) should contain only three elements.', function () {
+        foo = sym([1, 2, 5], [2, 3, 5], [3, 4, 5]);
+        //foo = [1, 4, 5];
+        expect(foo.length).toBe(3);
+    });
+    
+    it('sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]) should return [1, 4, 5]', function () {
+        foo = sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]);
+        //foo = [1, 4, 5];
+        expect(foo).toEqual([1, 4, 5]);
+    });
+    
+    it('sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]) should contain only three elements', function () {
+        foo = sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]);
+        //foo = [1, 4, 5];
+        expect(foo.length).toBe(3);
+    });
+    
+    it('sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]) should return [2, 3, 4, 6, 7]', function () {
+        foo = sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]);
+        //foo = [2, 3, 4, 6, 7];
+        expect(foo).toEqual([2, 3, 4, 6, 7]);
+    });
+    
+    it('sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]) should contain only five elements.', function () {
+        foo = sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]);
+        //foo = [2, 3, 4, 6, 7];
+        expect(foo.length).toBe(5);
+    });
+    
+    it('sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]) should return [1, 2, 4, 5, 6, 7, 8, 9]', function () {
+        foo = sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]);
+        //foo = [1, 2, 4, 5, 6, 7, 8, 9];
+        expect(foo).toEqual([1, 2, 4, 5, 6, 7, 8, 9]);
+    });
+    
+    it('sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]) should contain only eight elements', function () {
+        foo = sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]);
+        //foo = [2, 3, 4, 6, 7, 0, 0, 0];
+        expect(foo.length).toBe(8);
     });
   
 });
--- Symmetric Difference/src/challengeSymDiffSrc.js
+++ Symmetric Difference/src/challengeSymDiffSrc.js
@@ -1,5 +1,75 @@
-function sym(args) {
+function findSymDiff(setA, setB) {
     'use strict';
     
-    return args;
+    var symmetricDifference = [];
+
+    function isInANotB(value) {
+        var i;
+        
+        for (i = 0; i < setB.length; i += 1) {
+            if (value === setB[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function isInBNotA(value) {
+        var i;
+        
+        for (i = 0; i < setA.length; i += 1) {
+            if (value === setA[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    symmetricDifference = setA.filter(isInANotB).concat(setB.filter(isInBNotA));
+    symmetricDifference.sort(function (a, b) {
+        return a - b;
+    });
+    return symmetricDifference;
+}
+
+function destroyIntraDuplicates(set) {
+    'use strict';
+    
+    var i;
+    var processedSet = [];
+    
+    set.sort(function (a, b) {
+        return a - b;
+    });
+
+    for (i = 0; i < (set.length - 1); i += 1) {
+        if (set[i] !== set[i + 1]) {
+            processedSet.push(set[i]);
+        }
+    }
+    
+    processedSet.push(set[set.length - 1]);
+    return processedSet;
+}
+
+function sym() {
+    'use strict';
+    
+    var i;
+    var numberOfArguments = arguments.length;
+    var setA = arguments[0];
+    var setB = arguments[1];
+    
+    setA = destroyIntraDuplicates(setA);
+    
+    //Handle arguments a pair at a time
+    for (i = 0; i < numberOfArguments; i += 1) {
+        setB = destroyIntraDuplicates(setB);
+        setA = findSymDiff(setA, setB);
+        if ((i + 2) < numberOfArguments) {
+            setB = arguments[i + 2];
+        } else {
+            return setA;
+        }
+    }
 }
